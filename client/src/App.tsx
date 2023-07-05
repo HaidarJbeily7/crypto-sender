86b1fa1a6b16dc5353c13b19eed053750b986cf5
Haidar Jbeily <h.jbeily@innopolis.university>
2023-07-05 09:00:33 +0300

    Build(Structure): create base folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import {
+  Loader,
+  Welcome,
+  Navbar,
+  Services,
+  Transactions,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <>
+      <span>Haidar</span>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Transactions.tsx
@@ -0,0 +1,5 @@
+const Tranaactions = () => {
+  return <></>;
+};
+
+export default Tranaactions;
